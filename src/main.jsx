7fe5860c1bf0679579b2fb68db1a03ad4988ce0a
Ezequiel <ezea1994@hotmail.com>
2024-06-05 18:37:24 -0300

    VITE_KEY

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import './index.css'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxPKI5Hp4lc3vxf3Hqb-jWdv5PkiNG5-U",
-  authDomain: "entregareactezequielache.firebaseapp.com",
-  projectId: "entregareactezequielache",
-  storageBucket: "entregareactezequielache.appspot.com",
-  messagingSenderId: "909853643373",
-  appId: "1:909853643373:web:e1173bcb8587c5f72aa564",
-  measurementId: "G-S96PK5JK4R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
